f7b6afd02ae5606e522cb1755b6dd0ca0e27e3fc
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2023-04-28 15:05:47 +0100

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector('#ingredients')
+ingredients.forEach(
+  ingredient => {
+  const name = document.createElement('li');
+  name.classList.add('item')
+  name.textContent = ingredient;
+  list.appendChild(name)
+}
+)
