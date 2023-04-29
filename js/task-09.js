a54cbfb1442b51f039878c5a6c598f068f4872f3
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2023-04-29 15:10:20 +0100

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,14 @@
+const colorBtn = document.querySelector('.change-color')
+const outputColor = document.querySelector('.color')
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+  }
+    function onColorChange() {
+        document.body.style.backgroundColor = getRandomHexColor();
+        outputColor.innerHTML = getRandomHexColor();
+    }
+
+    colorBtn.addEventListener('click', onColorChange)
