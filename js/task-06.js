7a25ccd707fe5a2cc12af9f9356b157cdd7b4514
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2023-04-30 00:36:08 +0100

    task8

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const textInsert = document.querySelector('#validation-input')
 
 const onValid = event => {
     textInsert.classList.add('invalid')
-    if (event.currentTarget.value.length <= Number(textInsert.getAttribute('data-length')))
+    if (event.currentTarget.value.length === Number(textInsert.getAttribute('data-length')))
     {textInsert.classList.replace('invalid', 'valid')}}
 
 textInsert.addEventListener('blur', onValid)
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const submitForm = document.querySelector('.login-form')
+const email = document.querySelector('[name="email"]')
+const password = document.querySelector('[name="password"]')
 
+submitForm.addEventListener("submit", onFormSend);
+
+function onFormSend(event) {
+  event.preventDefault();
+  const elements = {email: email.value, password: password.value};
+  if (email.value === "" || password.value === "") {
+    alert("Please fill in all the fields!");
+  }
+console.log(elements);
+submitForm.reset();
+}
